0b15c4cad081419d3754daf99af46d9bc485bdde
Yew Brown <brown.voss32@gmail.com>
2025-10-21 18:28:50 +0100

    deleted font syling in attempt to fix cahracter issue

--- index.js
+++ index.js
@@ -46,7 +46,6 @@ http.createServer(function(req, res) {
            <title> MyPage </title>
            <style>
               body {
-                <!-- font-family: "Lucida Console", "Courier New", monospace; -->
                 background-color: #92aa86ff;
                 color: #ffd6f7ff;
                 margin: 33px;
