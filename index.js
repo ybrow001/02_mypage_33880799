1bcaadee87a009c2d85554d24b9f61746b761f93
Yew Brown <brown.voss32@gmail.com>
2025-10-21 18:26:46 +0100

    commented out font styling; try to fix issue - chinese characters not being displayed correctly

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ http.createServer(function(req, res) {
            <title> MyPage </title>
            <style>
               body {
-                font-family: "Lucida Console", "Courier New", monospace;
+                <!-- font-family: "Lucida Console", "Courier New", monospace; -->
                 background-color: #92aa86ff;
                 color: #ffd6f7ff;
                 margin: 33px;
